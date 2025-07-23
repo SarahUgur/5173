b5fac1b45fd21be24bec4f8246b00aa558272746
SarahUgur <sarahugur1@gmail.com>
2025-07-23 22:07:24 +0200

    Updated Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -173,8 +173,8 @@ export default function Header({
                 />
                 {currentUser?.isSubscribed && (
                   <div className="absolute -bottom-1 -right-1 w-4 h-4 bg-purple-600 rounded-full flex items-center justify-center">
-                    <Star className="w-3 h-3 mr-1" />
-                    Verificeret
+                    <Star className="w-3 h-3 text-white" />
+                  </div>
                 )}
               </button>
 
