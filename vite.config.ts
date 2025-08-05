5e7faf489486a69da358f155bc464175faae3ed4
SarahUgur <sarahugur1@gmail.com>
2025-08-05 21:56:00 +0200

    Fix proxy configuration for Netlify Dev

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: '0.0.0.0',
     port: 5175,
-    proxy: !process.env.VITE_NETLIFY_DEV ? {
+    proxy: !process.env.NETLIFY_DEV ? {
       '/api': {
         target: 'http://localhost:8888',
         changeOrigin: true,
