27f999ca7554c2a279234bf3733f195055574e2c
SarahUgur <sarahugur1@gmail.com>
2025-08-05 23:10:33 +0200

    Fix Vite proxy to use NETLIFY_DEV environment variable

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => {
     server: {
       host: '0.0.0.0',
       port: 5175,
-      ...(env.VITE_NETLIFY_DEV !== 'true' && {
+      ...(process.env.NETLIFY_DEV !== 'true' && {
         proxy: {
           '/api': {
             target: 'http://localhost:8888',
