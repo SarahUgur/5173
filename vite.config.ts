8ab128d70e5a41b467968374c885af8527abf4f4
SarahUgur <sarahugur1@gmail.com>
2025-08-06 12:37:38 +0200

    Fix Vite proxy to always be active

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   
-  return {
     plugins: [react()],
     server: {
       host: '0.0.0.0',
