667df2b7861cc0b44650890878589fde51aa3fa5
SarahUgur <sarahugur1@gmail.com>
2025-08-06 12:30:43 +0200

    Fix Vite config environment variable

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
-  const isNetlifyDev = process.env.NETLIFY_DEV === 'true';
+  const isNetlifyDev = env.VITE_NETLIFY_DEV === 'true';
   
   return {
     plugins: [react()],
